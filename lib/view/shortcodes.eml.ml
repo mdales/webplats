07d8df877bcd9a57b8eec9f627ffa873c0dea1e7
Michael Dales <michael@digitalflapjack.com>
2024-11-26 14:05:40 +0000

    Fix copy and paste error

--- lib/view/shortcodes.eml.ml
+++ lib/view/shortcodes.eml.ml
@@ -26,7 +26,7 @@ let render_youtube code =
     <div>
       <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/<%s! code %>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-  </div>⏎ 
+  </div>
 
 let render_shortcode shortcode =
   match shortcode with
