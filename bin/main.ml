029bd84588b06d498cba57e4ca71da0ae5312e0e
Michael Dales <michael@digitalflapjack.com>
2024-12-06 19:11:21 +0000

    Fix static location to match hugo

--- bin/main.ml
+++ bin/main.ml
@@ -185,14 +185,18 @@ let routes_for_taxonomies ~section_renderer ~page_renderer ~thumbnail_loader
 
 let collect_static_routes site =
   let website_dir = Site.path site in
+  let website_static_dir = website_dir / "static" in
   let theme_static_dir =
     website_dir / "themes" / Site.hugo_theme site / "static"
   in
 
-  let theme_things_to_be_published =
-    Sys.readdir (Fpath.to_string theme_static_dir)
-    |> Array.to_list
-    |> List.map (fun n -> theme_static_dir / n)
+  let things_to_be_published =
+    List.concat_map
+      (fun static_dir ->
+        Sys.readdir (Fpath.to_string static_dir)
+        |> Array.to_list
+        |> List.map (fun n -> static_dir / n))
+      [ website_static_dir; theme_static_dir ]
   in
 
   List.map
@@ -205,7 +209,7 @@ let collect_static_routes site =
             (Dream.static (Fpath.to_string (path / ".")))
       | false ->
           Dream.get ("/" ^ basename) (Dream.static (Fpath.to_string path)))
-    theme_things_to_be_published
+    things_to_be_published
 
 let () =
   let website_dir =
@@ -227,8 +231,6 @@ let () =
             |> List.sort (fun (_, a) (_, b) ->
                    Ptime.compare (Page.date b) (Page.date a)))
           |> Dream.html);
-      Dream.get "/static/**"
-        (Dream.static (Fpath.to_string (website_dir / "static" / ".")));
     ]
     @ collect_static_routes site
   in
