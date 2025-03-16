dd3b4070c20345b3def037874552618dc053a30a
Michael Dales <michael@digitalflapjack.com>
2025-03-16 14:00:22 +0000

    Move from Camlimages to imagemagick for image resizing

--- src/view/image.ml
+++ src/view/image.ml
@@ -26,7 +26,7 @@ let makedirs path =
              p)
            (Fpath.v hd) tl)
 
-let render_image_fill page filename (max_width, max_height) =
+let _render_image_fill page filename (max_width, max_height) =
   let imgpath = Fpath.add_seg (Page.path page) filename in
   let targetname =
     Printf.sprintf "image_fill_%dx%d_%s" max_width max_height filename
@@ -78,7 +78,39 @@ let render_image_fill page filename (max_width, max_height) =
       Images.save target_path None [] resultimg;
       Fpath.v target_path
 
-let render_image_fit page filename (max_width, max_height) =
+
+  let render_image_fill page filename (max_width, max_height) =
+    let imgpath = Fpath.add_seg (Page.path page) filename in
+    let targetname =
+      Printf.sprintf "image_fill_%dx%d_%s" max_width max_height filename
+    in
+    let target_folder =
+      Fpath.append (cache_dir ()) (Fpath.v (Page.url_name page))
+    in
+    let target_path = Fpath.to_string (Fpath.add_seg target_folder targetname) in
+    match Sys.file_exists target_path with
+    | true -> Fpath.v target_path
+    | false ->
+        makedirs target_folder;
+        let img = Images.load (Fpath.to_string imgpath) [] in
+        let width, height = Images.size img in
+        let fwidth = float_of_int width and fheight = float_of_int height in
+        let f_max_width = float_of_int max_width
+        and f_max_height = float_of_int max_height in
+        let ftarget =
+          match fwidth /. fheight > f_max_width /. f_max_height with
+          | true -> f_max_width
+          | false -> f_max_height
+        in
+        let wratio = ftarget /. fwidth and hratio = ftarget /. fheight in
+        let ratio = max wratio hratio in
+        let newwidth = int_of_float (ratio *. fwidth)
+        and newheight = int_of_float (ratio *. fheight) in
+        let cmd = Printf.sprintf "convert %s -gravity center -resize %dx%d^ -extent %dx%d %s" (Fpath.to_string imgpath) newwidth newheight newwidth newheight target_path in
+        let _rc = Unix.system cmd in
+        Fpath.v target_path
+
+let _render_image_fit page filename (max_width, max_height) =
   let imgpath = Fpath.add_seg (Page.path page) filename in
   let targetname =
     Printf.sprintf "image_fit_%dx%d_%s" max_width max_height filename
@@ -114,6 +146,36 @@ let render_image_fit page filename (max_width, max_height) =
           in
           Images.save target_path None [] resultimg;
           Fpath.v target_path)
+
+let render_image_fit page filename (max_width, max_height) =
+  let imgpath = Fpath.add_seg (Page.path page) filename in
+  let targetname =
+    Printf.sprintf "image_fit_%dx%d_%s" max_width max_height filename
+  in
+  let target_folder =
+    Fpath.append (cache_dir ()) (Fpath.v (Page.url_name page))
+  in
+  let target_path = Fpath.to_string (Fpath.add_seg target_folder targetname) in
+  match Sys.file_exists target_path with
+  | true -> Fpath.v target_path
+  | false -> (
+    makedirs target_folder;
+    let img = Images.load (Fpath.to_string imgpath) [] in
+    let width, height = Images.size img in
+    let fwidth = float_of_int width and fheight = float_of_int height in
+    let wratio = float_of_int max_width /. fwidth
+    and hratio = float_of_int max_height /. fheight in
+    match wratio >= 1.0 && hratio >= 1.0 with
+    | true -> imgpath
+    | false -> (
+      let ratio = min wratio hratio in
+      let newwidth = int_of_float (ratio *. fwidth)
+      and newheight = int_of_float (ratio *. fheight) in
+      let cmd = Printf.sprintf "convert %s -resize %dx%d %s" (Fpath.to_string imgpath) newwidth newheight target_path in
+      let _rc = Unix.system cmd in
+      Fpath.v target_path
+    )
+  )
 
 let render_image page filename scale (max_width, max_height) =
   match scale with
